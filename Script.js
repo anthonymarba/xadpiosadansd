2f32d60daec3ef551e5db551bcf3a4d23003c66f
Anthony <142261068+anthonymarba@users.noreply.github.com>
2024-10-02 12:26:44 -0300

    a

--- Script.js
+++ Script.js
@@ -1,16 +1,24 @@
-let estados =['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte','Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina','São Paulo','Sergipe','Tocantins']
+// let estados =['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte','Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina','São Paulo','Sergipe','Tocantins']
 
-let i = 0
+// let i = 0
 
 
-while (i < estados.length) {
-     console.log(i)
-     console.log(estados [i]);
-     i = i+1;
-}
+// while (i < estados.length) {
+//      console.log(i)
+//      console.log(estados [i]);
+//      i = i+1;
+// }
 
-for (let index = 0; index < estados.length; index++) {
-     console.log(index);
-     console.log(estados[index]);
+// for (let index = 0; index < estados.length; index++) {
+//      console.log(index);
+//      console.log(estados[index]);
      
+// }
+
+let idade = ;
+if(idade>17) {
+     console.log('maior de idade');
 }
+ else {
+     console.log('menor de idade');
+ }
